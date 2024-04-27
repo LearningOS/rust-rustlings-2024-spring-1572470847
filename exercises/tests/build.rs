f29cccf63fabaf8fb04dbccb9eaf14b996e91d9f
1572470847 <119294778+1572470847@users.noreply.github.com>
2024-04-27 16:05:45 +0000

    update

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -1,6 +1,4 @@
-//! This is the build script for both tests7 and tests8.
-//!
-//! You should modify this file to make both exercises pass.
+
 
 fn main() {
     // In tests7, we should set up an environment variable
